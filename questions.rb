75869ab10573e8e9b413391af169898b278303e5
Will Hall <willhall88@hotmail.com>
2014-05-23 10:43:21 +0100

    5 tests passing

--- questions.rb
+++ questions.rb
@@ -21,6 +21,7 @@ end
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
+  array.each{|element| element.reverse!}
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
--- questions_spec.rb
+++ questions_spec.rb
@@ -28,10 +28,10 @@ describe 'the Friday test :)' do
     expect(n).to eq ['a', 'b', 'c']
   end
 
-  # specify 'reverse_every_element_in_array' do
-  #   n = reverse_every_element_in_array ['dog', 'monkey', 'elephant']
-  #   expect(n).to eq ['god', 'yeknom', 'tnahpele']
-  # end
+  specify 'reverse_every_element_in_array' do
+    n = reverse_every_element_in_array ['dog', 'monkey', 'elephant']
+    expect(n).to eq ['god', 'yeknom', 'tnahpele']
+  end
 
   # specify 'every_possible_pairing_of_students' do
   #   n = every_possible_pairing_of_students(['Bob', 'Dave', 'Clive']) || []
