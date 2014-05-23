f97d1c0545b520dc3db7f2457e4fec847659e7f6
Will Hall <willhall88@hotmail.com>
2014-05-23 11:26:49 +0100

    turn a symbol to a string

--- questions_spec.rb
+++ questions_spec.rb
@@ -101,10 +101,10 @@ describe 'the Friday test :)' do
     expect(n).to eq [1, 2, 3, 1, 2, 3]
   end
 
-  # specify 'turn_symbol_into_string' do
-  #   n = turn_symbol_into_string :foobar
-  #   expect(n).to eq 'foobar'
-  # end
+  specify 'turn_symbol_into_string' do
+    n = turn_symbol_into_string :foobar
+    expect(n).to eq 'foobar'
+  end
 
   # specify 'average_of_array' do
   #   n = average_of_array [10, 15, 25]
