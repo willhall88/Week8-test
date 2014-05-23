ef231cfcd2500e497db12fb86869b8ccb274200f
Will Hall <willhall88@hotmail.com>
2014-05-23 11:26:04 +0100

    17 passes

--- questions_spec.rb
+++ questions_spec.rb
@@ -96,10 +96,10 @@ describe 'the Friday test :)' do
     expect(n).to eq 25
   end
 
-  # specify 'double_array' do
-  #   n = double_array [1, 2, 3]
-  #   expect(n).to eq [1, 2, 3, 1, 2, 3]
-  # end
+  specify 'double_array' do
+    n = double_array [1, 2, 3]
+    expect(n).to eq [1, 2, 3, 1, 2, 3]
+  end
 
   # specify 'turn_symbol_into_string' do
   #   n = turn_symbol_into_string :foobar
