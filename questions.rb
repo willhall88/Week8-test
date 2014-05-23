a0ba528f3e8462e41ab593dbebd555465faf2c45
Will Hall <willhall88@hotmail.com>
2014-05-23 11:34:20 +0100

    take all elements until greater than 5

--- questions.rb
+++ questions.rb
@@ -120,6 +120,7 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+  array.take_while{|element| element < 6}
 end
 
 # turn an array (with an even number of elements) into a hash, by
